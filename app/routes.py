54dc2e4990793528ead7253cf35ce0dd78419628
Piyush Maurya <piyushkumarmauyra@gmail.com>
2019-06-15 19:23:55 +0530

    Editor and compiler updates
    - Added time limit of 1s during code run time
    - Added codemirror plugin to code editor textarea
    - Changed logo
    - Changed title to PiCompiler
    - Added compiled to gitignore

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,9 @@ from app import app
 from app.forms import CppForm
 import subprocess
 import json
+from threading import Timer
 
+kill = lambda process: process.kill()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -31,12 +33,18 @@ def home():
                 error = ""
 
             subprocess.call(
-                ["./compiled/tempCode <compiled/tempInput >compiled/tempOutput"],
-                shell=True,
+                    ["./compiled/tempCode <compiled/tempInput >compiled/tempOutput"],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    shell=True,
+                    timeout=1
             )
+
             f = open("compiled/tempOutput", "r")
             output = f.readlines()
             f.close()
+        except subprocess.TimeoutExpired:
+            error = "Time Limit Exceeded"
         except Exception as e:
             error = str(e)
 
