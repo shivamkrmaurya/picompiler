c2ab6287ddac6553c25ea46faeb5a6240fa3c992
Piyush Maurya <piyushkumarmauyra@gmail.com>
2019-08-17 12:37:45 +0530

    Updated compile keycode, fixed height

--- app/templates/code.cpp
+++ app/templates/code.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 
+#define ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define ll long long
 #define endl '\n'
 #define pii pair<ll int, ll int>
@@ -8,11 +9,22 @@
 #define S second
 #define pb push_back
 #define mp make_pair
+#define repi(i, n) for(ll i=0; i<n; i++)
+#define rep(i, a, b) for(ll i=a; i<=b; i++)
 #define M 1000000007
 
 using namespace std;
 
 int main() {
-    cout<<"Hello World";
+    ios
+    ll t; 
+    t=1;
+    cin>>t;
+    while(t--){
+        ll n; cin>>n;
+        ll a[n];
+        rep(i, n) cin>>a[i];
+        
+    }
     return 0;
 }
